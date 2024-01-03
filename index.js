b606b872219fc503f45dfcb9871f875b2a23f4a5
Oza.m <sanarsanar86@gmail.com>
2024-01-03 18:34:16 +0300

    qw

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.get("/login", (req, res) => {
-  res.sendFile(__dirname.replace("Backend", "Frontend") + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/Frontend/index.html");
 });
 app.get("/back", (req, res) => {
-  res.sendFile(__dirname.replace("Backend", "Frontend") + "/back.html");
+  res.sendFile(__dirname + "/Frontend/back.html");
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on port: http://localhost:3000/home`);
+app.listen(22021, () => {
+  console.log(`Server is running on port: http://de1.bot-hosting.net:22021/`);
 });
