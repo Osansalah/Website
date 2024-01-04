7f978b5e39270c5f359ad2edfd8fdaef23fdcfd8
Oza.m <sanarsanar86@gmail.com>
2024-01-05 00:50:17 +0300

    login coockies

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const setupLoginRoute = require("./routes/Login.js");
+const setupAddRoute = require("./routes/add.js");
+const { config } = require("dotenv");
+const { cookieJwtAuth } = require("./middleware/cookieJwtAuth");
+
+const app = express();
+config();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+app.get("/", cookieJwtAuth, (req, res) => {
+  res.sendFile(__dirname + "/Frontend/html/Main.html");
+});
+app.get("/registration", (req, res) => {
+  res.sendFile(__dirname + "/Frontend/html/registration.html");
+});
+
+setupLoginRoute(app);
+setupAddRoute(app);
+
+app.listen(process.env.Port, () => {
+  console.log(
+    `Server is running on port: http://de1.bot-hosting.net:${process.env.Port}/ \n http://127.0.0.1:${process.env.Port}`
+  );
+});
